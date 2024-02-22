8d7c7e69bbf81c38713a1234c9da2097ce2d4ada
Moufid Magdy <106033993+moufid-magdy@users.noreply.github.com>
2024-02-22 19:15:24 +0200

    final

--- app/src/main/java/com/example/random_quote_generator_app/MainActivity.kt
+++ app/src/main/java/com/example/random_quote_generator_app/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.random_quote_generator_app
 
-import android.R
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
